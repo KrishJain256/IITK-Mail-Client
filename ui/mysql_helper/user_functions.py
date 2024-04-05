6a46afe80349bbf8e64a91ae4114fd258d88d19e
KrishJain256 <138436914+KrishJain256@users.noreply.github.com>
2024-04-06 00:58:29 +0530

    Update user_functions.py

--- ui/mysql_helper/user_functions.py
+++ ui/mysql_helper/user_functions.py
@@ -1,14 +1,14 @@
 import mysql.connector
 
 def create_new_user(username,password,firstname,lastname,email,rollno):
-    con = mysql.connector.connect(host="localhost", user="root", passwd="krish", database="mailclient")
+    con = mysql.connector.connect(host="localhost", user="root", passwd="{YOUR_PASSWORD}", database="mailclient")
     cursor = con.cursor()
     query = f"Insert into ui_user (username,password,firstname,lastname,email,rollno) values ('{username}','{password}','{firstname}','{lastname}','{email}',{rollno})"
     cursor.execute(query)
     con.commit()
 
 def verify_user(username,password):
-    con = mysql.connector.connect(host="localhost", user="root", passwd="krish", database="mailclient")
+    con = mysql.connector.connect(host="localhost", user="root", passwd="{YOUR_PASSWORD}", database="mailclient")
     cursor = con.cursor()
     query = f"select exists(select * from ui_user where username = '{username}' and password = '{password}')"
     cursor.execute(query)
@@ -18,7 +18,7 @@ def verify_user(username,password):
         return 0
 
 def send_mail(sender,receiver,subject,body):
-    con = mysql.connector.connect(host="localhost", user="root", passwd="krish", database="mailclient")
+    con = mysql.connector.connect(host="localhost", user="root", passwd="{YOUR_PASSWORD}", database="mailclient")
     cursor = con.cursor()
     query = "Insert into ui_mail (sender,receiver,subject,body,date_time) values('{}','{}','{}','{}',CURRENT_TIMESTAMP())".format(sender, receiver,
                                                                                                     subject, body)
