50a59cb10ba77ff8ca1b30110e56fe5c0da9ec18
KrishJain256 <138436914+KrishJain256@users.noreply.github.com>
2024-04-06 00:57:36 +0530

    Update dbconnect.py

--- ui/mysql_helper/dbconnect.py
+++ ui/mysql_helper/dbconnect.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 def estabilish_connection():
-    con = mysql.connector.connect(host="localhost", user="root", passwd="krish")
+    con = mysql.connector.connect(host="localhost", user="root", passwd="{YOUR_PASSWORD}")
     cursor = con.cursor()
     cursor.execute("create database mailclient")
     con.commit()
